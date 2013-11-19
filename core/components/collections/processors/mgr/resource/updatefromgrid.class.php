62cb7ef6d9bfca4cd74bec0997eb16904f24610e
Jan Peca <theboxercz@gmail.com>
2013-11-19 10:18:35 +0100

    Aded update from grid processor that saves the menu index

--- core/components/collections/processors/mgr/resource/updatefromgrid.class.php
+++ core/components/collections/processors/mgr/resource/updatefromgrid.class.php
@@ -1,6 +1,8 @@
 <?php
 
 class CollectionsUpdateFromGridProcessor extends modObjectUpdateProcessor {
+    public $classKey = 'modResource';
+
     public function initialize() {
         $data = $this->getProperty('data');
         if (empty($data)) return $this->modx->lexicon('invalid_data');
@@ -10,6 +12,47 @@ class CollectionsUpdateFromGridProcessor extends modObjectUpdateProcessor {
         $this->unsetProperty('data');
 
         return parent::initialize();
+    }
+
+    public function process() {
+        /* Run the beforeSet method before setting the fields, and allow stoppage */
+        $canSave = $this->beforeSet();
+        if ($canSave !== true) {
+            return $this->failure($canSave);
+        }
+
+        $this->object->set('menuindex', $this->getProperty('menuindex'));
+
+        /* Run the beforeSave method and allow stoppage */
+        $canSave = $this->beforeSave();
+        if ($canSave !== true) {
+            return $this->failure($canSave);
+        }
+
+        /* run object validation */
+        if (!$this->object->validate()) {
+            /** @var modValidator $validator */
+            $validator = $this->object->getValidator();
+            if ($validator->hasMessages()) {
+                foreach ($validator->getMessages() as $message) {
+                    $this->addFieldError($message['field'],$this->modx->lexicon($message['message']));
+                }
+            }
+        }
+
+        /* run the before save event and allow stoppage */
+        $preventSave = $this->fireBeforeSaveEvent();
+        if (!empty($preventSave)) {
+            return $this->failure($preventSave);
+        }
+
+        if ($this->saveObject() == false) {
+            return $this->failure($this->modx->lexicon($this->objectType.'_err_save'));
+        }
+        $this->afterSave();
+        $this->fireAfterSaveEvent();
+        $this->logManagerAction();
+        return $this->cleanup();
     }
 
 }
