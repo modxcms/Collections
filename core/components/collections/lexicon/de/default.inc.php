3bd2baeaf763635ce6d765d7df1e50706ff02934
Patrick Blank <patrickblank@Patricks-iMac.local>
2013-12-16 06:37:49 +0100

    Added German translation
    Added missing German translation

---
+++ core/components/collections/lexicon/de/default.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default German Lexicon Entries for Collections
+ *
+ * @package collections
+ * @subpackage lexicon
+ */
+$_lang['collections'] = 'Collections';
+
+// System lexicons
+$_lang['collections.system.type_name'] = 'Sammlung';
+$_lang['collections.system.text_create'] = 'Sammlung erstellen';
+$_lang['collections.system.text_create_here'] = 'Sammlung hier erstellen';
+$_lang['collections.system.new_container'] = 'Neue Sammlung';
+$_lang['collections.system.all'] = 'Alle';
+
+// Global lexicons
+$_lang['collections.global.search'] = 'Search';
+
+// Children
+$_lang['collections.children'] = 'Kinder';
+$_lang['collections.children.create'] = 'Kind erstellen';
+$_lang['collections.children.update'] = 'Kind aktualisieren';
+$_lang['collections.children.delete'] = 'Kind lšschen';
+$_lang['collections.children.delete_confirm'] = 'Sind Sie sicher, dass Sie dieses Kind lšschen mšchten?';
+$_lang['collections.children.publish_multiple'] = 'Auswahl veršffentlichen';
+$_lang['collections.children.unpublish_multiple'] = 'Auswahl zurŸckziehen';
+$_lang['collections.children.delete_multiple'] = 'Auswahl lšschen';
+$_lang['collections.children.delete_multiple_confirm'] = 'Sind Sie sicher, das Sie alle ausgewŠhlen Kinder lšschen mšchten?';
+$_lang['collections.children.undelete_multiple'] = 'Auswahl wiederherstellen';
+$_lang['collections.children.none'] = 'Diese Resource hat keine Kinder';
+$_lang['collections.children.err_ns_multiple'] = 'Sie mŸssen mindestens ein Kind auswŠhlen';
+$_lang['collections.children.menuindex'] = 'MenŸ Index';
