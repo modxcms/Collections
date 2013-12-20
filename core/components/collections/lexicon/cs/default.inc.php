cc6e6fc0e422b4a7d29eb77d89637eb1b35b53ca
Jan Peca <pecajan@gmail.com>
2013-12-20 16:57:13 +0100

    Fixed #12. Added czech translations.

---
+++ core/components/collections/lexicon/cs/default.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default Czech Lexicon Entries for Collections
+ *
+ * @package collections
+ * @subpackage lexicon
+ */
+$_lang['collections'] = 'Sbírky';
+
+// System lexicons
+$_lang['collections.system.type_name'] = 'Sbírka';
+$_lang['collections.system.text_create'] = 'Sbírku';
+$_lang['collections.system.text_create_here'] = 'Sbírku';
+$_lang['collections.system.new_container'] = 'Nová sbírka';
+$_lang['collections.system.all'] = 'Vše';
+
+// Global lexicons
+$_lang['collections.global.search'] = 'Hledat';
+
+// Children
+$_lang['collections.children'] = 'Potomci';
+$_lang['collections.children.create'] = 'Vytvořit potomka';
+$_lang['collections.children.update'] = 'Upravit potomka';
+$_lang['collections.children.delete'] = 'Odstranit potomka';
+$_lang['collections.children.delete_confirm'] = 'Jste si jisti, že chcete odstranit tohoto potomka?';
+$_lang['collections.children.publish_multiple'] = 'Publikovat vybrané';
+$_lang['collections.children.unpublish_multiple'] = 'Ukončit publikování vybraným';
+$_lang['collections.children.delete_multiple'] = 'Odstranit vybrané';
+$_lang['collections.children.delete_multiple_confirm'] = 'Jste si jisti, že chcete odstranit všechny vybrané potomky?';
+$_lang['collections.children.undelete_multiple'] = 'Obnovit vybrané';
+$_lang['collections.children.none'] = 'Tento dokument nemá žádné potomky.';
+$_lang['collections.children.err_ns_multiple'] = 'Musíte vybrat alespoň jednoho potomka.';
+$_lang['collections.children.menuindex'] = 'Index';
