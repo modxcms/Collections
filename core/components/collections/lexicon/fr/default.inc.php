23772509314ead409518792daaed2a9d454e7bb6
Romain <romain@melting-media.com>
2014-01-06 09:37:14 +0100

    French translation

---
+++ core/components/collections/lexicon/fr/default.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default French Lexicon Entries for Collections
+ *
+ * @package collections
+ * @subpackage lexicon
+ */
+$_lang['collections'] = 'Collections';
+
+// System lexicons
+$_lang['collections.system.type_name'] = 'Collection';
+$_lang['collections.system.text_create'] = 'Collection';
+$_lang['collections.system.text_create_here'] = 'Créer une nouvelle collection ici';
+$_lang['collections.system.new_container'] = 'Nouvelle collection';
+$_lang['collections.system.all'] = 'Tous';
+
+// Global lexicons
+$_lang['collections.global.search'] = 'Rechercher';
+
+// Children
+$_lang['collections.children'] = 'Enfants';
+$_lang['collections.children.create'] = 'Créer un enfant';
+$_lang['collections.children.update'] = 'Mettre à jour l\'enfant';
+$_lang['collections.children.delete'] = 'Supprimer l\'enfant';
+$_lang['collections.children.delete_confirm'] = 'Êtes-vous sûr de vouloir supprimer cet enfant ?';
+$_lang['collections.children.publish_multiple'] = 'Publier la sélection';
+$_lang['collections.children.unpublish_multiple'] = 'Dé-publier la sélection';
+$_lang['collections.children.delete_multiple'] = 'Supprimer la sélection';
+$_lang['collections.children.delete_multiple_confirm'] = 'Êtes-vous sûr de vouloir supprimer tous les enfants séletionnés ?';
+$_lang['collections.children.undelete_multiple'] = 'Restorer la sélection';
+$_lang['collections.children.none'] = 'Cette ressource n\'a pas d\'enfant.';
+$_lang['collections.children.err_ns_multiple'] = 'Vous devez sélectionner au moins un enfant.';
+$_lang['collections.children.menuindex'] = 'Index de menu';
