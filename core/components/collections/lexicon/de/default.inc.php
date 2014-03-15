4fe4a5c67365644b07bc38246d7bf72e236ba275
exside <luk@exside.ch>
2014-03-16 00:53:12 +0100

    German and English lexicon updates clean from dependencies

--- core/components/collections/lexicon/de/default.inc.php
+++ core/components/collections/lexicon/de/default.inc.php
@@ -7,6 +7,12 @@
  */
 $_lang['collections'] = 'Sammlungen';
 
+// Settings lexicons
+$_lang['setting_collections.mgr_date_format'] = 'Grid Datumsformat';
+$_lang['setting_collections.mgr_date_format_desc'] = 'Hier können Platzhalter für strftime() verwendet werden.';
+$_lang['setting_collections.mgr_time_format'] = 'Grid Zeitformat';
+$_lang['setting_collections.mgr_time_format_desc'] = 'Hier können Platzhalter für strftime() verwendet werden.';
+
 // System lexicons
 $_lang['collections.system.type_name'] = 'Sammlung';
 $_lang['collections.system.text_create'] = 'Sammlung erstellen';
@@ -15,19 +21,23 @@ $_lang['collections.system.new_container'] = 'Neue Sammlung';
 $_lang['collections.system.all'] = 'Alle';
 
 // Global lexicons
-$_lang['collections.global.search'] = 'Search';
+$_lang['collections.global.search'] = 'Suchen';
 
 // Children
-$_lang['collections.children'] = 'Kinder';
-$_lang['collections.children.create'] = 'Kind erstellen';
-$_lang['collections.children.update'] = 'Kind aktualisieren';
-$_lang['collections.children.delete'] = 'Kind löschen';
-$_lang['collections.children.delete_confirm'] = 'Sind Sie sicher, dass Sie dieses Kind löschen möchten?';
+$_lang['collections.children'] = 'Unterresourcen';
+$_lang['collections.children.create'] = 'Unterresource erstellen';
+$_lang['collections.children.update'] = 'Unterresource aktualisieren';
+$_lang['collections.children.delete'] = 'Unterresource löschen';
+$_lang['collections.children.delete_confirm'] = 'Sind Sie sicher, dass Sie diese Unterresource löschen möchten?';
 $_lang['collections.children.publish_multiple'] = 'Auswahl veröffentlichen';
 $_lang['collections.children.unpublish_multiple'] = 'Auswahl zurückziehen';
 $_lang['collections.children.delete_multiple'] = 'Auswahl löschen';
-$_lang['collections.children.delete_multiple_confirm'] = 'Sind Sie sicher, das Sie alle ausgewählen Kinder löschen möchten?';
+$_lang['collections.children.delete_multiple_confirm'] = 'Sind Sie sicher, das Sie alle ausgewählen Unterresourcen löschen möchten?';
 $_lang['collections.children.undelete_multiple'] = 'Auswahl wiederherstellen';
-$_lang['collections.children.none'] = 'Diese Resource hat keine Kinder';
-$_lang['collections.children.err_ns_multiple'] = 'Sie müssen mindestens ein Kind auswählen';
+$_lang['collections.children.none'] = 'Diese Resource hat keine Unterresourcen';
+$_lang['collections.children.err_ns_multiple'] = 'Sie müssen mindestens eine Unterresource auswählen';
 $_lang['collections.children.menuindex'] = 'Menü Index';
+
+$_lang['collections.err.parent_ns'] = 'Keine Elternresource definiert.';
+$_lang['collections.err.bad_sort_column'] = 'Der Grid muss nach <strong>[[+column]]</strong> sortiert sein um Drag & Drop Sortierung zu aktivieren.';
+$_lang['collections.err.clear_filter'] = 'Bitte setzen Sie <strong>Filter</strong> und <strong>Suche</strong> zurück um Drag & Drop Sortierung zu aktivieren.';
