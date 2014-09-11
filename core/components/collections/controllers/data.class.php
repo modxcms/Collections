9d016360a47b1b926f52fe6da86f1fe48b93823b
Jan Peca <pecajan@gmail.com>
2014-09-11 16:51:44 +0200

    Added data controller to show resource owerview page
    Resolves #61

---
+++ core/components/collections/controllers/data.class.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Loads the collection data page
+ *
+ * @package collections
+ * @subpackage controllers
+ */
+class CollectionContainerDataManagerController extends ResourceDataManagerController {}
