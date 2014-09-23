1fb0317f59ac913dcdd5c3ea912d27462ca223cd
Jan Peca <pecajan@gmail.com>
2014-09-23 13:16:24 +0200

    Improved close button in collection's childs

--- assets/components/collections/js/mgr/extra/hijackclose.js
+++ assets/components/collections/js/mgr/extra/hijackclose.js
@@ -1,16 +1,12 @@
-MODx.on("ready",function() {
-    var update = Ext.getCmp('modx-page-update-resource');
-    if (!update) {
-        return;
-    }
-    
-    var parentId = update.config.record.parent;
+Ext.override(MODx.page.UpdateResource, {
+    cancel: function(btn,e) {
+        var parentId = this.config.record.parent;
 
-    var cancelHandler = function(btn,e) {
         var fp = Ext.getCmp(this.config.formpanel);
         if (fp && fp.isDirty()) {
             Ext.Msg.confirm(_('warning'),_('resource_cancel_dirty_confirm'),function(e) {
                 if (e == 'yes') {
+                    fp.warnUnsavedChanges = false;
                     MODx.releaseLock(MODx.request.id);
                     MODx.sleep(400);
                     MODx.loadPage('resource/update', 'id='+parentId);
@@ -20,13 +16,5 @@ MODx.on("ready",function() {
             MODx.releaseLock(MODx.request.id);
             MODx.loadPage(MODx.action['resource/update'], 'id='+parentId);
         }
-    };
-
-    Ext.each(update.buttons, function(item, index) {
-        if (item.text == _('cancel')) {
-            item.handler = cancelHandler;
-
-            return false;
-        }
-    });
+    }
 });
