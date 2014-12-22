bf271c97429ca59ca551185288482d8eee9705ad
Jan Peca <pecajan@gmail.com>
2014-12-22 21:20:42 +0100

    Added system setting to modify image path in image renderer
    Thanks to Brian Wente for the idea

--- assets/components/collections/js/mgr/extra/collections.renderers.js
+++ assets/components/collections/js/mgr/extra/collections.renderers.js
@@ -61,6 +61,7 @@ Collections.renderer.timestampToDatetime = function(value, metaData, record, row
 
 Collections.renderer.image = function(value, metaData, record, rowIndex, colIndex, store) {
     if (value != '' && value != null) {
-        return '<img src="' + MODx.config.base_url + value + '" width="64">';
+        var imgPath = MODx.config['collections.renderer_image_path'];
+        return '<img src="' + MODx.config.base_url + imgPath + value + '" width="64">';
     }
 };
