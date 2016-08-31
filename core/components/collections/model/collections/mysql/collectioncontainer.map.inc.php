f0f72dba29626cc0872f2bc9ba54d943213f2418
Jan Peca <pecajan@gmail.com>
2016-08-31 17:11:29 +0200

    Collections 3.5.1

--- core/components/collections/model/collections/mysql/collectioncontainer.map.inc.php
+++ core/components/collections/model/collections/mysql/collectioncontainer.map.inc.php
@@ -2,23 +2,25 @@
 /**
  * @package collections
  */
-$xpdo_meta_map['CollectionContainer'] = array(
-    'package' => 'collections',
-    'version' => NULL,
-    'extends' => 'modResource',
-    'fields' =>
-        array(),
-    'fieldMeta' =>
-        array(),
-    'composites' =>
-        array(
-            'Setting' =>
-                array(
-                    'class' => 'CollectionSetting',
-                    'local' => 'id',
-                    'foreign' => 'collection',
-                    'cardinality' => 'one',
-                    'owner' => 'local',
-                ),
-        ),
+$xpdo_meta_map['CollectionContainer']= array (
+  'package' => 'collections',
+  'version' => NULL,
+  'extends' => 'modResource',
+  'fields' => 
+  array (
+  ),
+  'fieldMeta' => 
+  array (
+  ),
+  'composites' => 
+  array (
+    'Setting' => 
+    array (
+      'class' => 'CollectionSetting',
+      'local' => 'id',
+      'foreign' => 'collection',
+      'cardinality' => 'one',
+      'owner' => 'local',
+    ),
+  ),
 );
