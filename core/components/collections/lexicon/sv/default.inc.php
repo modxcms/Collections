25a76fccc51588ee079aa50f15d2fc36c30f7175
Vladimir <Skippidar@hotmail.com>
2017-11-23 13:28:37 +0300

    Where condition label

--- core/components/collections/lexicon/sv/default.inc.php
+++ core/components/collections/lexicon/sv/default.inc.php
@@ -133,6 +133,7 @@ $_lang['collections.template.child_searchable'] = 'Standard sökbar för barn';
 $_lang['collections.template.child_richtext'] = 'Standard riktext för barn';
 $_lang['collections.template.child_content_type'] = 'Standard innehållstyp för barn';
 $_lang['collections.template.parent'] = 'Parent-id (optional, @SNIPPET can be used)';
+$_lang['collections.template.child_selection'] = 'Where condition (optional, in JSON)';
 $_lang['collections.template.child_content_disposition'] = 'Default children\'s content disposition';
 $_lang['collections.template.sort_type'] = 'Sort type';
 $_lang['collections.template.permanent_sort_before'] = 'Permanent sort - Before';
