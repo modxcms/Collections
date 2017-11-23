969938060003ff272d5ed600d2b4683deecd4269
Vladimir <Skippidar@hotmail.com>
2017-11-23 13:25:45 +0300

    Where condition label

--- core/components/collections/lexicon/en/default.inc.php
+++ core/components/collections/lexicon/en/default.inc.php
@@ -133,6 +133,7 @@ $_lang['collections.template.child_searchable'] = 'Default children\'s searchabl
 $_lang['collections.template.child_richtext'] = 'Default children\'s richtext';
 $_lang['collections.template.child_content_type'] = 'Default children\'s content type';
 $_lang['collections.template.parent'] = 'Parent-id (optional, @SNIPPET can be used)';
+$_lang['collections.template.child_selection'] = 'Where condition (optional, in JSON)';
 $_lang['collections.template.child_content_disposition'] = 'Default children\'s content disposition';
 $_lang['collections.template.sort_type'] = 'Sort type';
 $_lang['collections.template.permanent_sort_before'] = 'Permanent sort - Before';
