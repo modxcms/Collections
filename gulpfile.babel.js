ba34933cb8afbbb94b62a3806d3efe0a0188702c
Ryan Thrash <ryan@modx.com>
2019-06-06 12:43:08 -0500

    Update to Gulp 4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,85 +13,16 @@ import pkg from './package.json';
 import webpackConfig from "./webpack.config.js";
 import merge from "merge-stream";
 import concat from "gulp-concat";
-
-import sass from 'gulp-ruby-sass';
+import sass from 'gulp-sass';
 import sassInlineSvg from 'gulp-sass-inline-svg';
 import svgmin from 'gulp-svgmin';
+import log from "fancy-log";
 
 const $ = gulpLoadPlugins();
 const libFolder = './_build/assets/lib';
 const sources = './_build/assets/js/**/*.js';
 
-gulp.task('default', ['build-web']);
-
-// Build for web
-gulp.task('build-web', ['webpack:build-web', 'css']);
-
-// Build for web + watch
-gulp.task('build-web-dev', ['webpack:build-web-dev', 'css'], () => {
-    gulp.watch([sources], ['webpack:build-web-dev']);
-    gulp.watch(['./_build/assets/sass/**/*.scss'], ['css']);
-});
-
-gulp.task('css', function() {
-    var sassStream = sass('./_build/assets/sass/collections.scss', {
-        style: 'compressed',
-        loadPath: [
-            './_build/assets/sass'
-        ]
-    });
-
-    var cssStream = gulp.src([
-    ]);
-
-    return merge(sassStream,cssStream)
-        .pipe(concat('fred_integration.css'))
-        .pipe(gulp.dest('./assets/components/collections/web'));
-});
-
-gulp.task('sass:svg', function(){
-    return gulp.src('./_build/assets/images/**/*.svg')
-        .pipe(svgmin()) // Recommend using svg min to optimize svg files first
-        .pipe(sassInlineSvg({
-            destDir: './_build/assets/sass/'
-        }));
-});
-
-// Run Babel only
-gulp.task('build-babel', ['clean'], () =>
-gulp.src([sources])
-    .pipe($.babel())
-    // Output files
-    .pipe(gulp.dest(libFolder))
-);
-
-// Clean folder
-gulp.task('clean', () =>
-del([`${libFolder}/**/*`])
-);
-
-// Webpack helper
-gulp.task('webpack:build-web', done => {
-    var env = {'BUILD_ENV': 'PROD', 'TARGET_ENV': 'WEB'};
-var taskName = 'webpack:build-web';
-// run webpack
-webpack(webpackConfig(env), onBuild(done, taskName));
-});
-
-// Webpack watch helper
-// create a single instance of the compiler to allow caching
-var webDevCompiler = null;
-gulp.task('webpack:build-web-dev', done => {
-    var env = {'BUILD_ENV': 'DEV', 'TARGET_ENV': 'WEB'};
-var taskName = 'webpack:build-web-dev';
-// build dev compiler
-if (!webDevCompiler) {
-    webDevCompiler = webpack(webpackConfig(env));
-}
-// run webpack
-webDevCompiler.run(onBuild(done, taskName));
-});
-
+// utilities
 function onBuild(done, taskName) {
     return (err, stats) => {
         if (err)
@@ -109,3 +40,82 @@ function setEnv(buildEnv) {
         }
     });
 }
+
+// Build for web + watch
+
+gulp.task("css", function() {
+    var sassStream = gulp
+        .src("./_build/assets/sass/collections.scss")
+        .pipe(sass({ errLogToConsole: true, outputStyle: "compressed" }))
+        .on("error", sass.logError);
+    
+//  var cssStream = gulp.src([
+//      "./node_modules/flatpickr/dist/flatpickr.min.css",
+//      "./node_modules/nouislider/distribute/nouislider.min.css"
+//  ]);
+    
+    return merge(sassStream/*, cssStream*/)
+        .pipe(concat("fred_integration.css"))
+        .pipe(gulp.dest("./assets/components/collections/web"));
+});
+        
+
+gulp.task("sass:svg", function() {
+return gulp
+    .src("./_build/assets/images/**/*.svg")
+    .pipe(svgmin()) // Recommend using svg min to optimize svg files first
+    .pipe(
+    sassInlineSvg({
+        destDir: "./_build/assets/sass/"
+    })
+    );
+});
+  
+// Clean folder
+gulp.task('clean', () =>
+    del([`${libFolder}/**/*`])
+);
+
+
+// Run Babel only
+gulp.task('build-babel', gulp.series('clean'), () =>
+    gulp
+    .src([sources])
+    .pipe($.babel())
+    // Output files
+    .pipe(gulp.dest(libFolder))
+);
+
+// Webpack helper
+gulp.task('webpack:build-web', done => {
+    var env = {'BUILD_ENV': 'PROD', 'TARGET_ENV': 'WEB'};
+    var taskName = 'webpack:build-web';
+    // run webpack
+    webpack(webpackConfig(env), onBuild(done, taskName));
+});
+
+
+// Build for web
+gulp.task('build-web', gulp.series('webpack:build-web', 'css'));
+
+
+// Webpack watch helper
+// create a single instance of the compiler to allow caching
+var webDevCompiler = null;
+gulp.task('webpack:build-web-dev', done => {
+    var env = {'BUILD_ENV': 'DEV', 'TARGET_ENV': 'WEB'};
+    var taskName = 'webpack:build-web-dev';
+    // build dev compiler
+    if (!webDevCompiler) {
+        webDevCompiler = webpack(webpackConfig(env));
+    }
+    // run webpack
+    webDevCompiler.run(onBuild(done, taskName));
+});
+
+gulp.task('build-web-dev', gulp.series('webpack:build-web-dev', 'css'), () => {
+    gulp.watch([sources], ['webpack:build-web-dev']);
+    gulp.watch(['./_build/assets/sass/**/*.scss'], ['css']);
+});
+
+gulp.task("default", gulp.series("build-web"));
