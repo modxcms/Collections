d945ffd2024f47945738e67ff0f72849c2e16239
Jan Peca <pecajan@gmail.com>
2019-08-22 10:35:42 +0200

    Run the fred integration only for fred version 1.1.o-pl and above

--- core/components/collections/model/collections/src/Events/FredBeforeRender.php
+++ core/components/collections/model/collections/src/Events/FredBeforeRender.php
@@ -4,9 +4,33 @@ namespace Collections\Events;
 
 class FredBeforeRender extends Event
 {
+    /** @var \Fred */
+    protected $fred;
+
+    /** @var bool */
+    protected $preventRun = false;
+
+    public function __construct($modx, &$scriptProperties)
+    {
+        parent::__construct($modx, $scriptProperties);
+
+        $corePath = $this->modx->getOption('fred.core_path', null, $this->modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/fred/');
+
+        $this->fred = $this->modx->getService(
+            'fred',
+            'Fred',
+            $corePath . 'model/fred/',
+            array(
+                'core_path' => $corePath
+            )
+        );
+
+        if ((!($this->fred instanceof \Fred)) || (version_compare(\Fred::VERSION, '1.1.0-pl') < 0)) $this->preventRun = true;
+    }
 
     public function run()
     {
+        if ($this->preventRun) return true;
 
         $assetsPath = $this->collections->getOption('assets_url', [], $this->modx->getOption('assets_url', null, MODX_ASSETS_URL) . 'components/collections/');
 
