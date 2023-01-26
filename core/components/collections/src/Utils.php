7b0dd69057f7b8db58b8ef5fd88fbacb3683d9f0
Marcus Pöhls <mp@seda.digital>
2023-01-26 10:18:44 +0100

    check if string and explode

--- core/components/collections/src/Utils.php
+++ core/components/collections/src/Utils.php
@@ -4,7 +4,10 @@ namespace Collections;
 class Utils {
     public static function explodeAndClean($array, $delimiter = ',', $keepDuplicates = 0)
     {
-        $array = explode($delimiter, $array);     // Explode fields to array
+        if (is_string($array)) {
+            $array = explode($delimiter, $array); // Explode fields to array
+        }
+
         $array = array_map('trim', $array);       // Trim array's values
 
         if ($keepDuplicates == 0) {
